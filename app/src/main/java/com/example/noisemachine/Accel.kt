33e619351e0804a83ff79a45959e555139815b47
Liam R Mitchell <liam.mitchell@uleth.ca>
2019-08-15 23:06:25 -0600

    got the code to the point where it pulls X Y Z values from accelerometer and displays on screen

--- app/src/main/java/com/example/noisemachine/Accel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.noisemachine
-
-
-/**
- * this is the class which pulls data from the accelerometer
- */
-class Accel {
-
-}
--- app/src/main/java/com/example/noisemachine/MainActivity.kt
+++ app/src/main/java/com/example/noisemachine/MainActivity.kt
@@ -1,31 +1,84 @@
 package com.example.noisemachine
 
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Half.toFloat
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , SensorEventListener {
 
-    val nameArray = arrayOf<String>("Accelerometer", "Microphone")
-
-    val infoArray = arrayOf<String>("this is where the accelerometer readout goes", "this is where the mic readout goes")
-
-    val imageArray = arrayOf<Int>(R.drawable.accel_icon, R.drawable.mic_icon)
+    private var mSensorManager: SensorManager? = null
+    private var mAccelerometer: Sensor? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //get resources
+        val nameArray = resources.getStringArray(R.array.hw_names)
+        val infoArray = resources.getStringArray(R.array.hw_values)
+        val imageArray = arrayOf<Int>(R.drawable.accel_icon, R.drawable.mic_icon)
+
+        // get reference of the service
+        mSensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        // focus in accelerometer
+        mAccelerometer = mSensorManager!!.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        // setup the window
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+
+        //define custom adapter
         val adapter = CustomListAdapter(this, nameArray, infoArray, imageArray)
 
+        //apply custom adapter
         data_list_view.adapter = adapter
 
+        //on-click functionality
         data_list_view.setOnItemClickListener { adapterView, view, position, id ->
             val itemAtPos = adapterView.getItemAtPosition(position)
             val itemIdAtPos = adapterView.getItemIdAtPosition(position)
             Toast.makeText(this, "Click on item at $itemAtPos : item id $itemIdAtPos", Toast.LENGTH_LONG).show()
         }
+
     }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event != null) {
+
+            val accelval1: String = event.values[0].toString()
+            val accelval2: String = event.values[1].toString()
+            val accelval3: String = event.values[2].toString()
+
+            val nameArray = resources.getStringArray(R.array.hw_names)
+            val infoArray = arrayOf<String>( accelval1, accelval2, accelval3 )
+            val imageArray = arrayOf<Int>(R.drawable.accel_icon, R.drawable.accel_icon, R.drawable.accel_icon)
+
+            val adapter = CustomListAdapter(this, nameArray, infoArray, imageArray)
+
+            //apply custom adapter
+            data_list_view.adapter = adapter
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mSensorManager!!.registerListener(this,mAccelerometer,
+            SensorManager.SENSOR_DELAY_GAME)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mSensorManager!!.unregisterListener(this)
+    }
+
 }
